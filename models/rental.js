ed27cc05930edab58e63df4d3e506e1d5e04becb
amundojo <amundojo@gmail.com>
2024-09-11 18:16:40 +0200

    Add clear descriptions of each code block. Remove redundant comments for foreign key definitions

--- models/rental.js
+++ models/rental.js
@@ -3,8 +3,13 @@ const sequelize = require('../config/database');
 const Vehicle = require('./vehicle');
 const User = require('./user');
 
+// Define the Rental model
 const Rental = sequelize.define('Rental', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
   userId: {
     type: DataTypes.INTEGER,
     references: {
@@ -19,12 +24,18 @@ const Rental = sequelize.define('Rental', {
       key: 'id'
     }
   },
-  rentalDate: { type: DataTypes.DATE, allowNull: false }
+  rentalDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
 }, {
+  // Enable timestamps to automatically manage 'createdAt' and 'updatedAt' fields
   timestamps: true
 });
 
+// Define associations
 Rental.belongsTo(User, { foreignKey: 'userId' });
 Rental.belongsTo(Vehicle, { foreignKey: 'vehicleId' });
 
+// Export the Rental model
 module.exports = Rental;
