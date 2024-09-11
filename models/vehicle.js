2d5fe5954ce6c8dae6163a4d3d535504e8351e92
amundojo <amundojo@gmail.com>
2024-09-11 18:21:39 +0200

    Add descriptive comments to key fields in the code

--- models/vehicle.js
+++ models/vehicle.js
@@ -3,11 +3,25 @@ const sequelize = require('../config/database');
 const VehicleType = require('./vehicleType');
 const VehicleColour = require('./vehicleColour');
 
+// Define the Vehicle model
 const Vehicle = sequelize.define('Vehicle', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  registrationNo: { type: DataTypes.STRING, allowNull: false },
-  make: { type: DataTypes.STRING, allowNull: false },
-  model: { type: DataTypes.STRING, allowNull: false },
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  registrationNo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  make: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  model: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
   colourId: {
     type: DataTypes.INTEGER,
     references: {
@@ -22,11 +36,22 @@ const Vehicle = sequelize.define('Vehicle', {
       key: 'id'
     }
   },
-  features: { type: DataTypes.STRING, allowNull: false },
-  lastServiceDate: { type: DataTypes.DATE, allowNull: false },
-  rented: { type: DataTypes.BOOLEAN, defaultValue: false }
+  features: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastServiceDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  rented: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false // Indicates whether the vehicle is currently rented
+  }
 }, {
+  // Enable timestamps for automatic 'createdAt' and 'updatedAt' fields
   timestamps: true
 });
 
+// Export the Vehicle model
 module.exports = Vehicle;
