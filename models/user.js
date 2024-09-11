607f5356599439827861a1146ab1e40f22f13234
amundojo <amundojo@gmail.com>
2024-09-11 18:18:34 +0200

    Improve alignment and spacing.

--- models/user.js
+++ models/user.js
@@ -1,14 +1,34 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
+// Define the User model
 const User = sequelize.define('User', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  fullName: { type: DataTypes.STRING, allowNull: false },
-  username: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.STRING, allowNull: false }
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true // Ensure that the username is unique
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false // Role can be used to distinguish user types like 'admin', 'customer', etc.
+  }
 }, {
+  // Enable timestamps to automatically handle 'createdAt' and 'updatedAt'
   timestamps: true
 });
 
+// Export the User model
 module.exports = User;
