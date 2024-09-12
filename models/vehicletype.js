7e2bf844c3ddff2d7d653b0ea0530ee2750a81b8
amundojo <amundojo@gmail.com>
2024-09-12 13:52:15 +0200

    Add comments

--- models/vehicletype.js
+++ models/vehicletype.js
@@ -1,11 +1,21 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
+// Define the VehicleType model
 const VehicleType = sequelize.define('VehicleType', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  name: { type: DataTypes.STRING, allowNull: false }
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true // Auto-incrementing primary key
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false // Vehicle type name must be provided
+  }
 }, {
+  // Enable timestamps to automatically manage 'createdAt' and 'updatedAt'
   timestamps: true
 });
 
+// Export the VehicleType model
 module.exports = VehicleType;
