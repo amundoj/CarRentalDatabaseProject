9efe88519ecb2ce52e5957efd1b6266f969a3f4c
amundojo <amundojo@gmail.com>
2024-09-12 13:55:00 +0200

    Clarify the user object and add comments

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', user: null });
+  res.render('index', {
+    title: 'Express',
+    user: null // Currently, no user is passed to the view
+  });
 });
 
+/* GET login page. */
 router.get('/login', function (req, res, next) {
-  res.render('login', { title: 'Express', user: null });
+  res.render('login', {
+    title: 'Express',
+    user: null // No user is passed on the login page
+  });
 });
 
-
+/* GET signup page. */
 router.get('/signup', function (req, res, next) {
-  res.render('signup', { title: 'Express', user: null });
+  res.render('signup', {
+    title: 'Express',
+    user: null // No user is passed on the signup page
+  });
 });
 
 module.exports = router;
-
