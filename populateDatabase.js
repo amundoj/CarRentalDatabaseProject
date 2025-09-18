b124d88aca3774ea8df4b70c628d46eedaefdf50
Amund Oliver Josefsen <amundojo@gmail.com>
2025-09-18 13:49:06 +0200

    Edit populateDatabase.js to use fs.readFileSync

--- populateDatabase.js
+++ populateDatabase.js
@@ -12,29 +12,46 @@ async function populateDatabase() {
     await sequelize.sync({ force: true });
 
     // Populate VehicleColours
-    const coloursData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'vehicleColours.json'), 'utf8'));
+    const coloursData = JSON.parse(
+      fs.readFileSync(path.join(__dirname, 'data', 'vehicleColours.json'), 'utf8')
+    );
     await VehicleColour.bulkCreate(coloursData);
 
     // Populate VehicleTypes
-    const typesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'vehicleTypes.json'), 'utf8'));
+    const typesData = JSON.parse(
+      fs.readFileSync(path.join(__dirname, 'data', 'vehicleTypes.json'), 'utf8')
+    );
     await VehicleType.bulkCreate(typesData);
 
     // Populate Vehicles
-    const vehiclesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'vehicles.json'), 'utf8'));
+    const vehiclesData = JSON.parse(
+      fs.readFileSync(path.join(__dirname, 'data', 'vehicles.json'), 'utf8')
+    );
     await Vehicle.bulkCreate(vehiclesData);
 
     // Populate Users with check for duplicates
-    const usersData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'users.json'), 'utf8'));
+    const usersData = JSON.parse(
+      fs.readFileSync(path.join(__dirname, 'data', 'users.json'), 'utf8')
+    );
+
     for (const userData of usersData) {
+      // Hash the password before creating
       const hashedPassword = await bcrypt.hash(userData.password, 10);
+
       const [user, created] = await User.findOrCreate({
         where: { username: userData.username },
-        defaults: userData
+        defaults: {
+          ...userData,
+          password: hashedPassword
+        }
       });
-      if (!created) {
-        console.log(`User ${user.username} already exists.`);
+
+      if (created) {
+        console.log(
+          `Created user ${user.username} with hash: ${user.password} (length: ${user.password.length})`
+        );
       } else {
-        console.log(`User ${user.username} created.`);
+        console.log(`User ${user.username} already exists.`);
       }
     }
 
